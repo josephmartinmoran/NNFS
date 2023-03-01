cd66084fc2bdf5a8f3afb4df693111544617b919
josephmartinmoran <josephmmoran11@gmail.com>
2023-03-01 17:41:07 -0500

    corrected

--- Derivatives.py
+++ Derivatives.py
@@ -28,30 +28,30 @@ for i in range(5):
     y1 = f(x1)
     y2 = f(x2)
 
-print((x1, y1), (x2, y2))
+    print((x1, y1), (x2, y2))
 
-# Derivative approximation and y-intercept for the tangent line
-approximate_derivative = (y2 - y1) / (x2 - x1)
-b = y2 - approximate_derivative * x2
+    # Derivative approximation and y-intercept for the tangent line
+    approximate_derivative = (y2 - y1) / (x2 - x1)
+    b = y2 - approximate_derivative * x2
 
-# We put the tangent line calculation into a function so we can call it
-# Multiple times for different values of x
-# approximate_derivative and b are constant for given function
-# thus calculated once above the function
-#def tangent_line(x):
-    #return approximate_derivative * x + b
+    # We put the tangent line calculation into a function so we can call it
+    # Multiple times for different values of x
+    # approximate_derivative and b are constant for given function
+    # thus calculated once above the function
+    #def tangent_line(x):
+        #return approximate_derivative * x + b
 
 
-# plotting the tangent line
-# +/- 0.9 to draw the tangent line on our graph
-# then we calculate the y for given x using the tangent line function
-# matplotlib will draw a line for us through these points
-to_plot = [x1 - 0.9, x1, x1 + 0.9]
+    # plotting the tangent line
+    # +/- 0.9 to draw the tangent line on our graph
+    # then we calculate the y for given x using the tangent line function
+    # matplotlib will draw a line for us through these points
+    to_plot = [x1 - 0.9, x1, x1 + 0.9]
 
-plt.scatter(x1, y1, c=colors[i])
-plt.plot([point for point in to_plot],
-         [approximate_tangent_line(point, approximate_derivative) for point in to_plot],
-         c=colors[i])
+    plt.scatter(x1, y1, c=colors[i])
+    plt.plot([point for point in to_plot],
+             [approximate_tangent_line(point, approximate_derivative) for point in to_plot],
+             c=colors[i])
 
-print('Approximate derivative for f(x)', f'where x = {x1} is {approximate_derivative}')
+    print('Approximate derivative for f(x)', f'where x = {x1} is {approximate_derivative}')
 plt.show()
