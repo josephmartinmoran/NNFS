941e53420cb0ffc6c32603c960abd2abdbc918f5
josephmartinmoran <josephmmoran11@gmail.com>
2023-03-04 18:48:36 -0500

    backpropagationEx

---
+++ Backward.py
@@ -0,0 +1,109 @@
+import numpy as np
+import math
+import nnfs
+from nnfs.datasets import vertical_data
+
+nnfs.init()
+
+# Dense Layer
+class Layer_Dense:
+
+    # Layer initialization
+    def __init__(self, n_inputs, n_neurons):
+        # Initialize weights and biases
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1,n_neurons))
+
+    # Forward pass
+    def forward(self, inputs):
+        # Calculate output values from inputs, weights and biases
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+    # Backward pass
+    def backward(self, dvalues):
+        # Gradients on parameters
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+        # Gradient on values
+        self.dinputs = np.dot(dvalues, self.weights.T)
+
+# ReLU activation
+class Activation_ReLU:
+
+    # Forward pass
+    def forward(self, inputs):
+        # Calculate output values from inputs
+        self.inputs = inputs
+        self.outputs = np.maximum(0, inputs)
+
+    # Backward Pass
+    def backward(self, dvalues):
+        # Since we need to modify the original variable
+        # Lets make a copy of the values first
+        self.dinputs = dvalues.copy()
+
+        # Zero gradient where input values were negative
+        self.dinputs[self.dinputs <= 0] = 0
+
+# Common loss class
+class Loss:
+    # Calculates the data and regulatization losses
+    # given model ouput and ground truth values
+    def calculate(self, output, y):
+
+        # Calculate sample losses
+        sample_losses = self.forward(output, y)
+
+        # Calculate mean loss
+        data_loss = np.mean(sample_losses)
+
+        # return loss
+        return data_loss
+
+# Cross Entropy Loss
+class Loss_CategoricalCrossentropy(Loss):
+
+    # Forward pass
+    def forward(self, y_pred, y_true):
+
+        # number of samples in a batch
+        samples = len(y_pred)
+
+        #Clip data to prevent division by 0
+        # Clip both sides to not drag mean towards any value
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        # Probabilities for target values - only if categorical labels
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[
+                range(samples),
+                y_true
+            ]
+
+        # Mask values - only for one-hot encoded labels
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(
+                y_pred_clipped*y_true,
+                axis=1
+            )
+
+        # Losses
+        negative_log_likelihood = -np.log(correct_confidences)
+        return negative_log_likelihood
+
+    # Backward pass
+    def backward(self,dvalues, y_true):
+
+        # Number of samples
+        samples = len(dvalues)
+        # Number of labels in ever sample
+        labels = len(dvalues[0])
+
+        # If labels are sparse, turn them into one-hot vector
+        if len(y_true.shape) == 1:
+            y_true = np.eye(labels)[y_true]
+
+        # Calculate gradient
+        self.dinputs = -y_true / dvalues
+        # Normalize gradient
+        self.dinputs = self.dinputs / samples
