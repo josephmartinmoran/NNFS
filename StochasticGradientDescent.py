Stochastic Gradient FIXED

StochasticGradientDescent.py
@@ -199,41 +199,6 @@ class Optimizer_SGD:
 
 optimizer = Optimizer_SGD()
 
-# Perform a forward pass of our training data through this layer
-dense1.forward(X)
-
-# Perform a forward pass through activation function
-# takes the output of first dense layer here
-activation1.forward(dense1.output)
-
-# Perform a forward pass through second Dense layer
-# takes outputs of activation function of first layer as inputs
-dense2.forward(activation1.ouput)
-
-# Perform a forward pass through the activation/loss function
-# takes outputs of second dense layer here and returns loss
-loss = loss_activation.forward(dense2.output, y)
-
-# Lets print loss value
-print('loss:', loss)
-
-# Calculate accuracy from output of activation2 and targets
-# calculate values along first axis
-predictions = np.argmax(loss_activation.output, axis=1)
-if len(y.shape) == 2:
-    y = np.argmax(y, axis=1)
-accuracy = np.mean(predictions == y)
-
-# Backward pass
-loss_activation.backward(loss_activation.output, y)
-dense2.backward(loss_activation.dinputs)
-activation1.backward(dense2.dinputs)
-dense1.backward(activation1.dinputs)
-
-# Update weights and biases
-optimizer.update_params(dense1)
-optimizer.update_params(dense2)
-
 # Create dataset
 X, y = spiral_data(samples=100, classes=3)
 
@@ -273,7 +238,7 @@ for epoch in range(10001):
 
     # Calculate accuracy from output of activation2 and targets
     # calculate values along first axis
-    predictions = np.argmax(loss_activation.ouput, axis=1)
+    predictions = np.argmax(loss_activation.output, axis=1)
     if len(y.shape) == 2:
         y = np.argmax(y, axis=1)
     accuracy = np.mean(predictions == y)